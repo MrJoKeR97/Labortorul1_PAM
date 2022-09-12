52d0b87c22232a00f446fc70e2cbf65fd0a86bfa
MrJoKeR97 <ioanlungu1204@gmail.com>
2022-09-12 15:35:08 +0300

    Laboratorul 1

--- app/src/main/java/com/example/laboratorul1/MainActivity.kt
+++ app/src/main/java/com/example/laboratorul1/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.laboratorul1
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,17 +29,19 @@ class MainActivity : AppCompatActivity() {
             if(user_field?.text?.toString()?.trim()?.equals("")!!)
                 Toast.makeText(this,"Introduceti textul", Toast.LENGTH_LONG).show()
             else {
-                val textul: String = user_field?.text.toString()
-                val te: String = textul.toMutableList().toString()
-                val at : String = te.lowercase()
-                val al  = at.count{it == 'a'}.toString()
-                result_info?.text = "Nr. literilor de a/A: ${al}"
-            }
+                var cuvinte  = 0
+                for( cuvint in user_field?.text.toString().split(" ")){
+                    if(cuvint.contains('a')|| cuvint.contains('A'))cuvinte++
+                }
+                result_info?.text = "Cuvintele cu litera  a/A: $cuvinte"
+                 }
             }
 
         }
 
     }
+
+
 
 
 
